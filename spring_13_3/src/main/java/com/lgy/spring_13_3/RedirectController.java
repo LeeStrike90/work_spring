588ce4a764c6ffdb79ed3b56811ff175d6677b7e
LEE <69142830+LeeStrike90@users.noreply.github.com>
2025-04-01 11:24:48 +0900

    redirect
    조건에 맞으면, redirect -> Ok.
    다르면, redirect -> Ng

---
+++ spring_13_3/src/main/java/com/lgy/spring_13_3/RedirectController.java
@@ -0,0 +1,37 @@
+package com.lgy.spring_13_3;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RedirectController
+{
+	@RequestMapping("/studentConfirm")
+	public String StudentRedirect(HttpServletRequest httpServletRequest)
+	{
+		String id = httpServletRequest.getParameter("id");
+	
+		if(id.equals("abc"))
+		{
+			return "redirect:studentOk";
+		}
+		else
+		{
+			return "redirect:studentNg";
+		}
+	}
+	
+	@RequestMapping("/studentOk")
+	public String studentOk()
+	{
+		return "student/studentOK";
+	}
+	@RequestMapping("/studentNg")
+	
+	public String studentNg()
+	{
+		return "student/studentNg";
+	}
+}
